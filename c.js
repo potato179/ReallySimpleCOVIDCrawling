9d0801a32d9771d92a05ffb212b706dee1bfae9d
White_KJ <53563618+CwhiteKJ@users.noreply.github.com>
2020-06-19 14:24:52 +0900

    Update c.js

--- c.js
+++ c.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const cheerio = require('cheerio')
 
 let url = 'https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query=%EC%BD%94%EB%A1%9C%EB%82%9819+%ED%99%95%EC%A7%84%EC%9E%90';
 request(url, function(error, response, body) {
